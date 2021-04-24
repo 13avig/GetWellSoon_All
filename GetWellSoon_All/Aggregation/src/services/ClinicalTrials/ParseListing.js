bb60c3306087ac7a2144aa809c1a9b680c88a4d0
13avig <13avig@gmail.com>
2021-04-23 21:26:31 -0700

    Aggregation

--- GetWellSoon_All/Aggregation/src/services/ClinicalTrials/ParseListing.js
+++ GetWellSoon_All/Aggregation/src/services/ClinicalTrials/ParseListing.js
@@ -9,7 +9,32 @@ class ParseListing extends PuppeteerHelper {
     let data = [];
     const page = await this.getNewPage();
     await page.goto("https://clinicaltrials.gov/ct2/results", { timeout: 60000 });
-    // TODO:: [A] need to add the remaining logic
+    await page.waitForSelector('#theDataTable tbody');
+    try {
+        let counter = 2;
+        while (true) {
+            data = [];
+            let reviewsElems = await page.$$('#theDataTable .odd td:nth-child(4),#theDataTable .even td:nth-child(4)');
+            await extractTrials(reviewsElems, data);
+            let nextPageLink = await page.$('#theDataTable_next span');
+
+            if (counter > 31500) {
+                break;
+            }
+            counter++;
+            console.log(counter);
+            await nextPageLink.click();
+            await page.waitFor(1000);
+        }
+
+        con.release;
+
+        await page.close();
+    }
+    catch (e) {
+        console.log(e);
+    }
+    return data;
     await page.close();
     return data;
   }
